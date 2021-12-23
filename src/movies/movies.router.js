37230dfb6ce8164b4cd2853201ac319539102fa4
LindseyinTech <lindseyintech@gmail.com>
2021-12-22 22:20:08 -0800

    updated movies.router

--- src/movies/movies.router.js
+++ src/movies/movies.router.js
@@ -3,18 +3,20 @@ const controller = require("./movies.controller");
 const methodNotAllowed = require("../errors/methodNotAllowed");
 
 
-router
-  .route("/:movieId/theaters")
+router.route("/:movieId/theaters")
   .get(controller.readTheaters)
   .all(methodNotAllowed);
 
-router
-  .route("/:movieId/reviews")
+router.route("/:movieId/reviews")
   .get(controller.readReviews)
   .all(methodNotAllowed);
 
-router.route("/:movieId").get(controller.read).all(methodNotAllowed);
+router.route("/:movieId")
+  .get(controller.read)
+  .all(methodNotAllowed);
 
-router.route("/").get(controller.list).all(methodNotAllowed);
+router.route("/")
+  .get(controller.list)
+  .all(methodNotAllowed);
 
 module.exports = router;
